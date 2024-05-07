c2b3d476b5e12d238f35abce956191779edcb506
DuyHungIT <167564878+DuyHungIT@users.noreply.github.com>
2024-05-07 14:52:03 +0700

    add

---
+++ hugn2/hung2.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Nguyen Duy Hung 2")
+}
--- hung1.go
+++ hung1.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Print("Hungoats")
+	fmt.Print("Nguyen Duy Hung")
 }
